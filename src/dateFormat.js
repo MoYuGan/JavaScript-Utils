95a693ecd511fa5fcb45aa3293f637d58d9c6cd9
liangzhicong <liangzhicong1@pconline.com.cn>
2020-12-03 14:41:26 +0800

    增加时间格式化函数

---
+++ src/dateFormat.js
@@ -0,0 +1,30 @@
+/**
+ * 时间格式化函数
+ * @param {时间格式} fmt 
+ * @param {时间} date 
+ */
+function dateFormat(fmt, date) {
+    const opt = {
+        'y+': date.getFullYear().toString(),        // 年
+        'M+': (date.getMonth() + 1).toString(),     // 月
+        'd+': date.getDate().toString(),            // 日
+        'h+': date.getHours().toString(),           // 时
+        'm+': date.getMinutes().toString(),         // 分
+        's+': date.getSeconds().toString()          // 秒
+    }
+    Object.keys(opt).forEach(key => {
+        let ret
+        ret = new RegExp('(' + key + ')').exec(fmt)
+        if (ret) {
+            fmt = fmt.replace(ret[1], ret[1].length == 1 ? opt[key] : opt[key].padStart(ret[1].length, '0'))
+        }
+    })
+    return fmt
+}
+
+function testDateFormat() {
+    const fmtDate =  dateFormat('yyyy-MM-dd hh:mm:ss', new Date())
+    console.log('fmtDate', fmtDate)
+}
+
+testDateFormat()
